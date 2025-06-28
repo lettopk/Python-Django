42462cf2546db95d3f4e1779e9a04279b1e58385
Pk Letto <mtarazona97@uan.edu.co>
2025-06-28 18:01:04 -0500

    variavles y propiedades, bucles y condicionales, filtros y cargadores, shortcuts y plantillas incrustadas

--- Projecto1/Projecto1/settings.py
+++ Projecto1/Projecto1/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Projecto1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['C:/Users/marlo/Favorites/python-django/Projecto1/Projecto1/plantillas'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Projecto1/Projecto1/views.py
+++ Projecto1/Projecto1/views.py
@@ -1,19 +1,55 @@
 from django.http import HttpResponse
 import datetime
-from django.template import Template, Context
+#from django.template import Template, Context
+#from django.template.loader import get_template
+from django.shortcuts import render
 
+class persona(object):
+    
+    def __init__(self, nombre, apellido):
+        self.nombre = nombre
+        self.apellido = apellido
 
 ##Creando la primer view
-def saludo(request):            #Primera vista
-    
-    doc_externo =  open("C:/Users/marlo/Favorites/python-django/Projecto1/Projecto1/plantillas/plantilla.html")          #llamada del archivo html
-    
-    plt= Template(doc_externo.read())                                   # se crea objeto template
-    doc_externo.close()                                                 #Se cierra documento
-    ctx = Context()                                                     # se crea el contexto
-    documento = plt.render(ctx)                                         #se renderiza el documento
-    return HttpResponse(documento)
 
+def saludo(request):            #Primera vista
+    p1 = persona("Marlon" , "Tarazona")                                         #se crea el objeto persona para mostrar
+
+    temas_curso = ["Plantillas","Modelos","Formularios","Vistas","Despliegue"]  #Lista para mostrar
+
+    ahora = datetime.datetime.now()                                             #variable simple con llamado de datetime
+                #(request, "nombre_plantilla", contexto)                        Prametros de render
+    return render(request, 'plantilla.html', {"nombre_persona":p1.nombre,
+                   "apellido_persona":p1.apellido, 
+                   "hora_actual": ahora, 
+                   "temas":temas_curso})
+    
+    """
+      #Metodo basico para llamar y crrear una plantilla_________________________________________________________________________________________________________________________
+        
+            doc_externo =  open("C:/Users/marlo/Favorites/python-django/Projecto1/Projecto1/plantillas/plantilla.html")  #llamada del archivo html, carga la plantilla
+                                                                                #Se envia la ruta a "DIRS" en setting.py
+            plt= Template(doc_externo.read())                                   # se crea objeto template
+            doc_externo.close()                                                 #Se cierra documento
+            
+            ctx = Context({"nombre_persona":p1.nombre,
+                           "apellido_persona":p1.apellido, 
+                           "hora_actual": ahora, 
+                           "temas":temas_curso})                            # se crea el contexto, donde se puede agregar un diccionario con la variable a mostrar
+            
+            documento = plt.render(ctx)
+            return #HttpResponse(documento)
+        
+      #Metodo rapido de cargado de plantillas___________________________________________________________________________________________________________________________________   
+            doc_externo = get_template('plantilla.html')     #Se utiliza el cargador de plantillas
+            documento = doc_externo.render({"nombre_persona":p1.nombre,
+                           "apellido_persona":p1.apellido, 
+                           "hora_actual": ahora, 
+                           "temas":temas_curso})                                         #se renderiza el documento
+            
+            Simplificacion de codigo con shortcuts____________________________________________________________________________________________________________________________
+            return HttpResponse(documento)
+"""
 
 def despedida(request):
     return HttpResponse('Adios mundo con django')
